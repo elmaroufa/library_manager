b9fac9d22659c2ce2392a41dd7db1764ff341792
elmaroufa <elmaroufa@gmail.com>
2022-08-25 11:52:19 +0100

    Feature inheritance delegation

--- models/library_book.py
+++ models/library_book.py
@@ -138,3 +138,7 @@ class LibraryMember(models.Model):
 
     _inherits = {'res.partner': 'partner_id'}
     partner_id = fields.Many2one('res.partner', ondelete='cascade')
+    date_start = fields.Date('Member Since')
+    date_end = fields.Date('Termination date')
+    member_number = fields.Char()
+    date_of_birth = fields.Date('Date of birth')
